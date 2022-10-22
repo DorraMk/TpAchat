11ae4ce081eca78b64295c3a27a095a529b10963
DorraMk <dorra.mkadem@esprit.tn>
2022-10-22 15:42:16 +0100

    mypart3

---
+++ src/main/java/com/tpAchat/service/IFactureService.java
@@ -0,0 +1,24 @@
+package com.tpAchat.service;
+
+import java.util.Date;
+import java.util.List;
+
+import com.tpAchat.entities.Facture;
+
+
+public interface IFactureService {
+	List<Facture> retrieveAllFactures();
+
+	List<Facture> getFacturesByFournisseur(Long idFournisseur);
+
+	Facture addFacture(Facture f);
+
+	void cancelFacture(Long id);
+
+	Facture retrieveFacture(Long id);
+	
+	void assignOperateurToFacture(Long idOperateur, Long idFacture);
+
+	float pourcentageRecouvrement(Date startDate, Date endDate);
+
+}
--- src/main/java/com/tpAchat/service/IntFournisseurService.java
+++ src/main/java/com/tpAchat/service/IntFournisseurService.java
@@ -1,5 +1,21 @@
 package com.tpAchat.service;
 
-public interface IntFournisseurService {
+import java.util.List;
 
+import com.tpAchat.entities.Fournisseur;
+
+
+
+public interface IntFournisseurService {
+	List<Fournisseur> retrieveAllFournisseurs();
+
+	Fournisseur addFournisseur(Fournisseur f);
+
+	void deleteFournisseur(Long id);
+
+	Fournisseur updateFournisseur(Fournisseur f);
+
+	Fournisseur retrieveFournisseur(Long id);
+	
+	void assignSecteurActiviteToFournisseur(Long idSecteurActivite, Long idFournisseur);
 }
